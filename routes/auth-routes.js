09ee53c4f4ed09d71870ce06f05d9028190e7dad
neitoo <samuraa855@gmail.com>
2024-05-02 22:04:00 +0500

    Реализованы роуты апи

---
+++ routes/auth-routes.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import AuthController from "../controllers/auth-controller.js";
+import AuthValidator from "../validations/valid.js";
+import UserController from "../controllers/user-controller.js";
+
+const router = Router();
+
+router.post("/auth", AuthValidator.signIn, AuthController.signIn);
+router.post("/registration", AuthValidator.signUp, AuthController.signUp);
+router.post("/logout", AuthValidator.logout, AuthController.logout);
+router.post("/refresh", AuthValidator.refresh, AuthController.refresh);
+
+router.post("/a/user",UserController.getInfoUser);
+
+export default router;
