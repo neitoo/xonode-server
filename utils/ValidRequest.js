0e53fe3edca49a3ad55aacea64690b0a316dc3d0
neitoo <samuraa855@gmail.com>
2024-05-02 22:05:23 +0500

    Реализованы кастомные вызовы ошибок

---
+++ utils/ValidRequest.js
@@ -0,0 +1,14 @@
+import ErrorUtils, {Unprocessable} from "./Errors.js";
+
+export default async (req,res,next,schema) => {
+    try {
+        if (schema) await schema.validate(req);
+
+        return next();
+    } catch ({path,errors}) {
+        return ErrorUtils.catchError(
+            res,
+            new Unprocessable(`${errors[0]}`)
+        );
+    }
+}
